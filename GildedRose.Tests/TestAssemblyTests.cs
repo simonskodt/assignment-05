ce7f83a8ff77d7c32286f390e96a99e39fa10554
Simon Johann Skødt <74598690+simonskodt@users.noreply.github.com>
2021-10-15 13:17:13 +0200

    Initial commit

---
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class TestAssemblyTests
+    {
+        [Fact]
+        public void TestTheTruth()
+        {
+            Assert.True(true);
+        }
+    }
+}
