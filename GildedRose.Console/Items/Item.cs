253c02710263ae8269273ec1c71ddb4f9fa6350a
Simon Johann Skødt <simon@skodt.dk>
2021-10-20 17:00:30 +0200

    Created classes to be refactored

---
+++ GildedRose.Console/Items/Item.cs
@@ -0,0 +1,11 @@
+namespace Item
+{
+    public class Item
+    {
+        protected string name;
+        protected int sellIn;
+        protected int quality;
+        
+        protected void Tick() {}
+    }
+}
